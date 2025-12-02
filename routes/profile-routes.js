29bb4e640cd0315bf00954c7b12670be8165dca7
JessPoseidon <104754709+JessPoseidon@users.noreply.github.com>
2025-12-02 16:31:14 +0800

    Update profile-routes.js

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-
-// --- Placeholder Routes for the server to load ---
 const authCheck = (req, res, next) => {
     if (!req.user) {
         res.redirect('/auth/login');
@@ -8,10 +6,8 @@ const authCheck = (req, res, next) => {
         next();
     }
 };
-
 router.get('/', authCheck, (req, res) => {
     // This route will render the protected dashboard
     res.send('Welcome to your profile! (Needs dashboard.ejs view)');
 });
-
 module.exports = router;
